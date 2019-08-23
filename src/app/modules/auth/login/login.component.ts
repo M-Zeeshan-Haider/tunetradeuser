5938429e81fe192762df414b38c26be4db030bfb
abdullah6265 <abdullahmasood186@gmail.com>
2019-08-23 19:34:09 +0500

    ui changes

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth-service/auth.service';
 import { LoginRequest } from 'src/app/shared/models';
@@ -9,33 +8,27 @@ import { LoginRequest } from 'src/app/shared/models';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
+  loginObj: any = {};
   email: any;
-  constructor(private fb: FormBuilder, private router: Router, private authservice: AuthService) { }
+  constructor( private router: Router, private authservice: AuthService) { }
 
   ngOnInit() {
-    this.createForm();
   }
 
-  createForm() {
-    this.loginForm = this.fb.group({
-      userId: ['', [Validators.required, Validators.email]],
-      userPassword: ['', [Validators.required]]
-    });
-  }
   login(data) {
     console.log('in login in login component result: ' + data);
     const loginData: LoginRequest = data;
-    this.authservice.login(loginData).subscribe(
+    this.authservice.login(this.loginObj).subscribe(
       res => {
         if (res.status === true) {
           this.router.navigate(['/user']);
           sessionStorage.setItem('authToken', res.token);
+          // tslint:disable-next-line:prefer-for-of
           for (let index = 0; index < res.data.length; index++) {
             this.email = res.data[index].userEmail;
-            
+
           }
-          localStorage.setItem('setemail', this.email)
+          localStorage.setItem('setemail', this.email);
         }
       }
     );
